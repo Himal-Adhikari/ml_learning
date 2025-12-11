8c9f8d7f3fe71bfbfe799f29edfca60e1fcf301c
Himal Adhikari <himalad321@gmail.com>
2025-12-11 09:00:45 +0545

    Decision Tree on Moons Dataset

---
+++ .ipynb_checkpoints/flake-checkpoint.nix
@@ -0,0 +1,35 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.pyright
+
+            pkgs.bashInteractive
+              (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+              matplotlib
+              numpy
+              pandas
+              sklearn-compat
+              jupyter
+              zipfile2
+            ]))
+          ];
+        };
+      }
+    );
+}
